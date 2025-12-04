e268eafced410008d2a4823180ec75cad64e0b7c
20220495-gif <20220495@sungshin.ac.kr>
2025-12-04 15:43:38 +0900

    fix: 아카이브 페이지 작품 업로드 버튼 연결

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// 1. 만들어둔 페이지들을 전부 불러옵니다 (Import)
-// (주의: 파일들이 pages 폴더 안에 있다고 가정했습니다. 만약 같은 폴더에 있다면 ./pages/를 지우고 ./만 남기세요)
+// 1. 페이지 컴포넌트 불러오기
 import Explore from './pages/Explore';
 import MySpace from './pages/MySpace';
 import Archive from './pages/Archive';
@@ -11,18 +10,19 @@ import ArchiveDetail from './pages/ArchiveDetail';
 import Login from './pages/Login';
 import Marketplace from './pages/Marketplace';
 import MarketplaceDetail from './pages/MarketplaceDetail';
-import MySpaceFolder from './pages/MySpaceFolder'; // 필요하다면 추가
-import MySpaceNode from './pages/MySpaceNode';     // 필요하다면 추가
+import MySpaceFolder from './pages/MySpaceFolder';
+import MySpaceNode from './pages/MySpaceNode';
 import MySpaceSetting from './pages/MySpaceSetting';
 import Setting from './pages/Setting';
 import SignUp from './pages/SignUp';
+import Upload from './pages/Upload'; // [추가] 업로드 페이지 import
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* 2. 주소(path)와 파일(element)을 연결해줍니다 (Route) */}
+          {/* 2. 주소(path)와 파일(element) 연결 */}
           
           {/* 메인 화면 */}
           <Route path="/" element={<Explore />} />
@@ -41,6 +41,9 @@ function App() {
           {/* 아카이브 관련 */}
           <Route path="/archive" element={<Archive />} />
           <Route path="/archive/detail" element={<ArchiveDetail />} />
+
+          {/* [추가] 업로드 페이지 */}
+          <Route path="/upload" element={<Upload />} />
 
           {/* 설정 및 로그인 */}
           <Route path="/setting" element={<Setting />} />
